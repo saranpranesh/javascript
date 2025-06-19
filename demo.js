8529f84a3d8fd74b2eb771b776a4dad6997e3155
saranpranesh <saranjagadeesh93@gmail.com>
2025-06-19 17:37:39 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,17 @@
+var age=21
+var isWithParents=true
+var hasIdProof=false
+
+if(age>=23){
+  console.log("Allow to Mall")
+}
+if(isWithParents){
+  console.log("Watch movie")
+}else{
+  console.mall("dont allow to watch movie")
+}
+if(hasIdProof){
+  console.log("Buy items")
+}else{
+  console.log("Dont allow to buy items")
+}
