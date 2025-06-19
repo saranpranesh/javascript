a95d83f8ed011d35b6c5d2129b5fa43e4f42f485
saranpranesh <saranjagadeesh93@gmail.com>
2025-06-19 18:22:23 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,21 @@
+
+// for loop
+
+for (i=0; i<=6; i++){
+    console.log("Enter a number" + i)
+}
+
+//while loop
+
+ let i = 0
+ while(i<=6){
+    console.log("Enter a number" + i)
+i++
+ }
+
+ // Do While Loop
+  let i = 7;
+do {
+  console.log("Enter a number " + i);
+  i++;
+} while (i <= 6);
